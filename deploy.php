01be06d5fa25345c1ee79edd63059c5b3a868966
R0n <pl4g4@users.noreply.github.com>
2015-12-02 21:32:07 -0600

    small changes

--- deploy.php
+++ deploy.php
@@ -11,7 +11,7 @@ if($_GET['token'] != 'pl4g4'){
 }
 
 $gitUrl = 'git@bitbucket.org:user/repo.git';
-$gitRepoFolder = /var/www/domain/public_html/public/repo/.git
+$gitRepoFolder = '/var/www/domain/public_html/public/repo/.git';
 $targetFolder = '/var/www/domain/public_html/';
 $repoFolder = '/var/www/domain/public_html/public/repo/';
 
@@ -19,7 +19,7 @@ try{
         print_r('<h1>Beginning Deployment...</h1><br />');
         exec('git clone -b development '. $gitUrl, $output);
         print_r('<h3>Repository cloned, deleting .git directory</h3><br />');
-        exec('rm -rf '.$gitFolder, $output);
+        exec('rm -rf '.$gitRepoFolder, $output);
         print_r('<h3>Removed .git directory, copying code from temporary location</h3><br />');
         exec('rsync -a '. $repoFolder . $targetFolder ,$output);
         print_r('<h3>Copied code from temporary location, removing temporary location</h3><br />');
